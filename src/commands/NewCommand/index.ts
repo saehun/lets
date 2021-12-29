01bff9cbc0295584bbfddbc91c8d1ed19049a0f6
minidonut <nycom13@gmail.com>
2021-12-29 22:38:48 +0900

    feat: impl New Command

--- src/commands/NewCommand/index.ts
+++ src/commands/NewCommand/index.ts
@@ -1,4 +1,10 @@
 import { Command, Option } from 'clipanion';
+import { loadGithubConfig } from '../../core/config/github.config';
+import { HandleKnownException } from '../../core/decorators/HandleKnownException';
+import { TaskRunner } from '../../core/task/task.runner';
+import { TemplateTask } from '../../core/task/template.task';
+import { TemplateLoader } from '../../core/template/template.loader';
+import { ask } from '../../utils/prompt.utils';
 
 export class NewCommand extends Command {
   static paths = [['new']];
@@ -10,10 +16,28 @@ export class NewCommand extends Command {
 
   template = Option.String({ required: false });
   name = Option.String({ required: false });
-  help = true;
+  packageManager = Option.String('--package-manager', 'npm', { description: '`npm` or `yarn`. default `npm`' });
+  git = Option.Boolean('--git', true, { description: 'Initialize git, default `true`' });
 
+  @HandleKnownException()
   async execute() {
-    this.context.stdout.write(`Hello ${this.template} ${this.name}!\n`);
+    const taskRunner = new TaskRunner();
+    const template = await new TemplateLoader().load(this.template);
+    const projectName = this.name ?? (await ask('project name?'));
+
+    const context = {
+      github: loadGithubConfig(),
+      project: { name: projectName },
+    };
+
+    await taskRunner
+      .register(new TemplateTask(template, projectName, context))
+      .register(new ChangeDirectoryTask(projectName))
+      .register(new DependencyTask(this.packageManager))
+      .register(new GitInitializeTask(this.git))
+      .register(new GreetingTask(projectName))
+      .run();
+
     return 0;
   }
 }
