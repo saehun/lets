10a78d38150db66c0114a4fa3d884264e3984664
minidonut <nycom13@gmail.com>
2021-12-29 22:36:42 +0900

    feat: impl `task runner`

---
+++ src/core/task/task.runner.ts
@@ -0,0 +1,36 @@
+import { TaskFailureError, TaskNotAvailableError } from '../error';
+import { Task } from './task';
+
+export class TaskRunner {
+  private readonly tasks: Task[] = [];
+
+  register(task: Task): TaskRunner {
+    this.tasks.push(task);
+    return this;
+  }
+
+  async run(): Promise<void> {
+    for (const task of this.tasks) {
+      if (typeof task.canExecute === 'function') {
+        const { available, reason } = await task.canExecute();
+        if (!available) {
+          throw new TaskNotAvailableError(task.name, reason);
+        }
+      }
+    }
+
+    const onErrorStack: Array<() => Promise<void>> = [];
+
+    try {
+      for (const task of this.tasks) {
+        onErrorStack.push(task.onError.bind(task));
+        await task.execute();
+      }
+    } catch (e) {
+      for (const onError of onErrorStack) {
+        await onError();
+      }
+      throw new TaskFailureError(e);
+    }
+  }
+}
