ba032635015b1c77c85017be2ac018b74b5a24dc
minidonut <nycom13@gmail.com>
2021-12-29 22:36:53 +0900

    feat: add prompt utils

--- src/utils/prompt.utils.ts
+++ src/utils/prompt.utils.ts
@@ -1,5 +1,18 @@
+import prompts from 'prompts';
 import { AbortException } from '../core/error';
 
 export function onAboart(): never {
-  throw new AbortException(`bye bye 👋`);
+  throw new AbortException();
+}
+
+export async function ask(message: string) {
+  const { result } = await prompts(
+    {
+      type: 'text',
+      name: 'result',
+      message,
+    },
+    { onCancel: onAboart }
+  );
+  return result;
 }
