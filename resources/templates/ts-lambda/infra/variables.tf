b02b64ff377d84d2284f5f54d6d78dfd74ddab77
minidonut <nycom13@gmail.com>
2022-01-01 06:26:57 +0900

    feat(ts-lambda): remove default values, which provided by env

--- resources/templates/ts-lambda/infra/variables.tf
+++ resources/templates/ts-lambda/infra/variables.tf
@@ -2,24 +2,20 @@
 
 variable "aws_region" {
   description = "AWS region for all resources."
-  type    = string
-  default = "ap-northeast-2"
+  type        = string
 }
 
 variable "lambda_function_name" {
   description = "Value of the Name for the lambda function"
   type        = string
-  default     = "ExampleHandler"
 }
 
 variable "s3_lambda_bucket" {
   description = "Lambda bucket to store lambda archive"
   type        = string
-  default     = "your-s3-bucket"
 }
 
 variable "s3_lambda_key" {
   description = "Lambda key to store lambda archive"
   type        = string
-  default     = "path/to/lambda.zip"
 }
